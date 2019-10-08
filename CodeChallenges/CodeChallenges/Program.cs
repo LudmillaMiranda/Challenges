6a720578147ba7a6a33e4e721bb4933a9e983f35
Cassia Almeida <lisa.shiphrah@gmail.com>
2019-10-08 11:35:23 +0100

    Added files for new project

---
+++ CodeChallenges/CodeChallenges/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenges
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Call here the funcion you want to run
+        }
+    }
+}
